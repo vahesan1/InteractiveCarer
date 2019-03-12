b8c1d2263218f8c913c7ddf92d6bbe4b8fd85145
vahesan <vahesan28@gmail.com>
2019-03-12 10:54:41 +0000

    Client Login/Registration Working

--- app/src/main/java/com/example/interactivecarer/ClientLogin.java
+++ app/src/main/java/com/example/interactivecarer/ClientLogin.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ClientLogin extends AppCompatActivity {
     DatabaseHelper db;
     TextView mRegisterClient;
     TextView mHome;
+    EditText mClientUsername;
+    EditText mClientPassword;
+    Button mClientScreenLogin;
 
 
     @Override
@@ -21,6 +26,11 @@ public class ClientLogin extends AppCompatActivity {
         db = new DatabaseHelper(this);
         mRegisterClient = (TextView) findViewById(R.id.clientregister);
         mHome = (TextView)findViewById(R.id.home);
+        mClientUsername = (EditText)findViewById(R.id.clientusername);
+        mClientPassword = (EditText)findViewById(R.id.clientpassword);
+        mClientScreenLogin = (Button)findViewById(R.id.clientscreenlogin);
+
+
         mRegisterClient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +43,25 @@ public class ClientLogin extends AppCompatActivity {
             public void onClick(View v) {
                 Intent mainScreen = new Intent(ClientLogin.this,MainActivity.class);
                 startActivity(mainScreen);
+            }
+        });
+
+        mClientScreenLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String clientUsername = mClientUsername.getText().toString().trim();
+                String clientPassword = mClientPassword.getText().toString().trim();
+                Boolean res = db.checkClient(clientUsername,clientPassword);
+                if (res == true)
+                {
+                    Toast.makeText(ClientLogin.this,"Successfully Logged In",Toast.LENGTH_SHORT).show();
+                    Intent mClientHome = new Intent(ClientLogin.this,ClientHome.class);
+                    startActivity(mClientHome);
+                }
+                else
+                {
+                    Toast.makeText(ClientLogin.this,"Login Error",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/interactivecarer/ClientRegister.java
+++ app/src/main/java/com/example/interactivecarer/ClientRegister.java
@@ -4,15 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ClientRegister extends AppCompatActivity {
+    DatabaseHelper db;
     TextView mAlreadyClient;
+    EditText nClientUsername;
+    EditText nClientPassword;
+    EditText nClientPasswordCnf;
+    Button nButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_register);
+        db = new DatabaseHelper(this);
         mAlreadyClient = (TextView)findViewById(R.id.clientregisteredalready);
+        nClientUsername = (EditText)findViewById(R.id.nclientusername);
+        nClientPassword = (EditText)findViewById(R.id.nclientpassword);
+        nClientPasswordCnf = (EditText)findViewById(R.id.nclientpassword2);
+        nButton = (Button)findViewById(R.id.clientregisterbutton);
         mAlreadyClient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -20,5 +33,33 @@ public class ClientRegister extends AppCompatActivity {
                 startActivity(loginClientScreen);
             }
         });
+
+        nButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String nClientUser = nClientUsername.getText().toString().trim();
+               String nClientPass = nClientPassword.getText().toString().trim();
+               String nClientPassCnf = nClientPasswordCnf.getText().toString().trim();
+
+               if (nClientPass.equals(nClientPassCnf)){
+                    Long val = db.addClient(nClientUser,nClientPass);
+                    if( val > 0){Toast.makeText(ClientRegister.this,"Successfully Registered",Toast.LENGTH_SHORT).show();
+                        Intent successfulRegister = new Intent(ClientRegister.this,ClientLogin.class);
+                        startActivity(successfulRegister);
+
+                    }
+                    else {
+
+                            Toast.makeText(ClientRegister.this, "Registration Error/Already Registered", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+               else{
+                   Toast.makeText(ClientRegister.this,"Password do not match",Toast.LENGTH_SHORT).show();
+
+               }
+            }
+        });
     }
 }
+
